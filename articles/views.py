a606eaa6b7b63bc726bbc9c88cbe9065de4b7231
Rishabh Singh <ris8645208@gmail.com>
2023-07-30 20:27:46 +0530

    feat: render article to template

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def article_list(request):
-  return render(request, 'articles/index.html')
+  # Get list of Articles
+  articles = Article.objects.all().order_by('date')
+  return render(request, 'articles/index.html', {
+    "articles": articles
+  })
