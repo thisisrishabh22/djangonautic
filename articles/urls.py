26fa0ba002379cda4bab97240fd3e042d11e33fe
Rishabh Singh <ris8645208@gmail.com>
2023-07-30 21:39:29 +0530

    feat: add articles show page

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.article_list)
+  path('', views.article_index),
+  path('/<slug>', views.article_show)
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
-def article_list(request):
+def article_index(request):
   # Get list of Articles
   articles = Article.objects.all().order_by('date')
   return render(request, 'articles/index.html', {
     "articles": articles
+  })
+
+def article_show(request, slug):
+  article = Article.objects.get(slug=slug)
+
+  if article == None:
+    return render(request, '404.html')
+
+  return render(request, 'articles/show.html', {
+    'article': article
   })
